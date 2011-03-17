5ddde42d88775d94b60dd888687c4fb1e9d226b2
Sarah Allen <sarah@ultrasaurus.com>
2011-03-16 18:00:19 -0700

    scopes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
-class Person < ActiveRecord::Base
+class Person < ActiveRecord::Base 
+  scope :all_present, where(:present => true)
+  scope :all_absent, where(:present => false)
+  scope :not_checked_in, where(:present => nil)
+  
   def full_name
     "#{first_name} #{last_name}"
   end
